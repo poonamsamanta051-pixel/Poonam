50d9ee83be20419cee99a35bdd358c8b5d47d35e
poonamsamanta051-pixel <poonamsamanta051@gmail.com>
2026-09-22 14:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
                             
 
-a=st.number_input("enter a number")
+a=st.number_input("enter a name")
 b=st.number_input("Enter another number")
 if st.button("add"):
    st.success(a+b)
